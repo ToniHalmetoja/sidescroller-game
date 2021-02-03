cda2fc3020c0288e0cf4c76f5b3cabf53173ca6b
Toni Halmetoja <36439969+ToniHalmetoja@users.noreply.github.com>
2021-02-03 23:29:39 +0100

    Update with obstacle
    Currently having no luck at making it move.

--- script.js
+++ script.js
@@ -1,59 +1,76 @@
-
 const gameArea = document.getElementById("gameArea");
 const ctx = gameArea.getContext("2d");
 
-var hero = new component("black", 10, 30, 30, 120);
-var obstacle = new component("black", 5, 20, 200, 130); 
+var hero = new component("black", 7, 30, 30, 120);
+var obstacles = [];
+
+
+
+var frameNo = 0;
+
 document.addEventListener("keydown", movement);
 
-var updater = setInterval(updateGameArea,20);
+var updater = setInterval(updateGameArea, 20);
+
+function everyInterval(n) {
+    if ((frameNo / n) % 1 == 0) {
+        return true;
+    }
+    return false;
+}
 
 function updateGameArea() {
+    var x, y;
+
     ctx.clearRect(0, 0, gameArea.width, gameArea.height);
-    obstacle.x -= 1;
-    console.log(obstacle.x);
-    obstacle.update();
+    frameNo += 1;
+    if (frameNo == 1 || everyInterval(50)) {
+        x = gameArea.width;
+        y = gameArea.height - 200
+        obstacles.push(new component("black", 5, 20, 300, 130));
+        console.log(obstacles);
+    }
+    for (i = 0; i < obstacles.length; i += 1) {
+        obstacles[i].x += -2;
+        obstacles[i].update();
+    }
     hero.update();
-    
+
 }
 
 function component(color, width, height, x, y) {
     this.width = width;
     this.height = height;
     this.x = x;
-    this.y = y;    
-    this.update = function() {
+    this.y = y;
+    this.update = function () {
         ctx.fillStyle = color;
-        ctx.fillRect(x, y, width, height);
+        ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-    this.move = function() {
-        if(y==120){
+    this.move = function () {
+        if (hero.y == 120) {
             let goingDown = false;
-            let jump = setInterval(function(){
-                if(y>=75 && goingDown == false){
-                    y-=5;
+            let jump = setInterval(function () {
+                if (hero.y >= 75 && goingDown == false) {
+                    hero.y -= 5;
                     hero.update();
-                    if(y==75){
-                        goingDown=true;
+                    if (hero.y == 75) {
+                        goingDown = true;
                     }
+                } else if (hero.y <= 120 && goingDown == true) {
+                    hero.y += 5;
                 }
 
-                else if(y<=120 && goingDown==true){
-                    y+=5;
-                    hero.update();
-
-                }            
-                
-                if(y==120 && goingDown==true){
+                if (hero.y == 120 && goingDown == true) {
                     clearInterval(jump);
                 }
-            },25);
+            }, 25);
         }
     }
 }
 
-function movement(evt){
-    if(evt.key=="1") {
-            hero.move();    
+function movement(evt) {
+    if (evt.key == "1") {
+        hero.move();
     }
 }
